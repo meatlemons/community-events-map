92d22587d8c841c5d1deef868820fcf533a92226
Oliver Webb <oliver_webb@trimble.com>
2022-03-22 11:43:50 +0000

    PRE-PROJECT PREP
    - generate map component and center on Ipswich

--- community-events-map/src/app/app.module.ts
+++ community-events-map/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
